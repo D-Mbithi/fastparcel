ac30913e155d6ccc9d8060dc2c414126fd71d4e1
dennis mbithi <denmbithi@gmail.com>
2022-03-17 14:34:44 +0300

    Added auth function

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'index.html')
+
+@login_required()
+def customer(request):
+    return render(request, 'index.html')
+
+@login_required()
+def courier(request):
+    return render(request, 'index.html')
